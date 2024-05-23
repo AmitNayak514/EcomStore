05382efca1e552e5bb9b10925fedb8ccf2b23f84
FlaSH514 <amitbabaninayak@gmail.com>
2024-05-24 00:51:06 +0530

    Category Page

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -1,8 +1,14 @@
 import { Button } from "./ui/button";
 
-const IconButton = () => {
+interface IconButtonProps {
+  icon: React.ReactElement;
+}
+
+const IconButton: React.FC<IconButtonProps> = ({ icon }) => {
   return (
-    <Button className="rounded-full flex items-center justify-center bg-white border shadow-sm p-2 hover:scale-110 transition"></Button>
+    <Button className="rounded-full flex items-center justify-center bg-white border shadow-sm p-2 hover:scale-110 transition">
+      {icon}
+    </Button>
   );
 };
 
