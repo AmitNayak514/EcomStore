77fc487055a4f703428aaea037c19cd92b030e91
FlaSH514 <amitbabaninayak@gmail.com>
2024-05-23 13:36:04 +0530

    Store Setup

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import { Urbanist } from "next/font/google";
+
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+import "./globals.css";
+
+/* import ModalProvider from "@/providers/modal-provider";
+import ToastProvider from "@/providers/toast-provider";
+
+import Footer from "@/components/footer"; */
+
+const font = Urbanist({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Store",
+  description: "Store - The place for all your purchases.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        {/* <ToastProvider />
+        <ModalProvider />*/}
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ components/IconButton.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./ui/button";
+
+const IconButton = () => {
+  return (
+    <Button className="rounded-full flex items-center justify-center bg-white border shadow-sm p-2 hover:scale-110 transition"></Button>
+  );
+};
+
+export default IconButton;
